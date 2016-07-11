d889ccf825dadc0deb3707ea14ff54f5ca014840
r7kamura <r7kamura@gmail.com>
2016-07-11 21:48:01 +0900

    Update badge on notifications fetched

--- src/chrome/js/background.js
+++ src/chrome/js/background.js
@@ -1,7 +1,40 @@
 import BackgroundProcess from '../../lib/background-process';
 
+class BadgeView {
+  /**
+   * @params {Object} props
+   */
+  constructor(props) {
+    this.props = props || {};
+  }
+
+  getText() {
+    if (this.props.notifications && this.props.notifications.totalCount > 0) {
+      return this.props.notifications.totalCount.toString();
+    } else {
+      return '';
+    }
+  }
+
+  render() {
+    chrome.browserAction.setBadgeBackgroundColor({
+      color : [
+        0,
+        152,
+        204,
+        255
+      ]
+    });
+    chrome.browserAction.setBadgeText({
+      text : this.getText()
+    });
+  }
+}
+
 const process = new BackgroundProcess();
+const badgeView = new BadgeView();
 process.onStateChanged(({ notifications }) => {
-  alert(notifications.totalCount);
+  badgeView.props.notifications = notifications;
+  badgeView.render();
 });
 process.start();
