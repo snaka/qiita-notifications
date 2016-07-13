c8eb6bbc31d421574c684da401ebe168f9ecdf88
r7kamura <r7kamura@gmail.com>
2016-07-13 14:47:16 +0900

    Fix so that we can use React

---
+++ src/chrome/js/popup.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// const backgroundPage = chrome.extension.getBackgroundPage();
+// console.log(backgroundPage);
+
+ReactDOM.render(
+  (<div>Hello world</div>),
+  document.getElementById('container')
+);
